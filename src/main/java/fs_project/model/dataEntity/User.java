fdd1ea649e93a966d8ce2c2c6c1174a1b174814c
Robin Vold <robvold98@gmail.com>
2021-05-13 11:12:28 +0200

    lagde litt på bruker

--- src/main/java/fs_project/model/dataEntity/User.java
+++ src/main/java/fs_project/model/dataEntity/User.java
@@ -1,4 +1,48 @@
 package fs_project.model.dataEntity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // bruk IDENTITY hvis db er seeded ved hver kjoering
+    private Long id;
+
+    private String name;
+    private String password;
+
+    public User(Long id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
